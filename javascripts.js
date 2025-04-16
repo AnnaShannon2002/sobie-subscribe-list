77532d3d77e1dbc670d3581664ac9c56f3b27c52
mtrejo03 <millydtrejo@gmail.com>
2025-04-16 12:06:32 -0500

    Update javascripts.js

--- javascripts.js
+++ javascripts.js
@@ -1,25 +1,22 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
+$(() => {
   $("#outputCard").hide();
-  
+
   'use strict';
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
   const forms = document.querySelectorAll('.needs-validation');
 
-  // Loop over them and prevent submission
   Array.from(forms).forEach(form => {
     form.addEventListener('submit', event => {
       if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
+        event.preventDefault();
+        event.stopPropagation();
       } else {
         event.preventDefault();
-        
+
         const userData = {
-        first: $("#validationCustom01").val(),
-        last: $("#validationCustom02").val(),
-        email: $("#validationCustomEmail").val()
+          first: $("#validationCustom01").val(),
+          last: $("#validationCustom02").val(),
+          email: $("#validationCustomEmail").val()
         };
 
         $("#outputBox").html(`
@@ -33,7 +30,8 @@
         console.log("Submitted User Info:", userData);
       }
 
-      form.classList.add('was-validated')
+      form.classList.add('was-validated');
     }, false);
   });
 });
+
